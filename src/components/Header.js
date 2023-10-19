02939e69e1f069a170a37d1886a3b0cd985da529
AndreMarrero <andremjr65@gmail.com>
2023-10-19 12:44:55 -0400

    finished project!

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,16 @@ import { NavLink } from "react-router-dom";
 
 function Header () {
     return (
-        <header>
+        <header id ="header">
             <h1 id= "title">
                 HolidaysRUS
             </h1>
-            <NavLink to="/Thanksgiving">Thanksgiving Selection</NavLink>
-            <NavLink to="/Christmas">Christmas Selection</NavLink>
-            <NavLink to="/Halloween">Halloween Selection</NavLink>
-            <NavLink to="/">Home Page</NavLink>
+            <div id="Navlinks">
+             <NavLink className="Navlink" to="/Thanksgiving">Thanksgiving Selection</NavLink>
+             <NavLink className="Navlink" to="/Christmas">Christmas Selection</NavLink>
+             <NavLink className="Navlink" to="/Halloween">Halloween Selection</NavLink>
+             <NavLink className="Navlink" to="/">Home Page</NavLink>
+            </div>
         </header>
         
     )
