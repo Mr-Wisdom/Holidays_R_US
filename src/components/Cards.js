c103bb5d6b24d7ce1a4c12bd0d413d377c42bf67
AndreMarrero <andremjr65@gmail.com>
2023-10-19 13:20:20 -0400

    finished project hurray!

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ function Cards ({name,price,holiday,image}) {
             <img src={image} alt = {name} className="card-image"/>
             <h3 className="card-title">{name}</h3>
             <p className="card-price">${price}</p>
-            <span className = "holiday">{holiday}</span>
+            <p className = "card-holiday">{holiday}</p>
         </li>
     )
 }
